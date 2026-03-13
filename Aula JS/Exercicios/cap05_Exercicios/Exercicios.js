2abbad37de121525c3c05041891481c1fa887583
Carla Vitória <carlavitoria0626@gmail.com>
2026-03-13 10:41:11 -0300

    Refactor fruit display program in Exercicios.js

--- Aula JS/Exercicios/cap05_Exercicios/Exercicios.js
+++ Aula JS/Exercicios/cap05_Exercicios/Exercicios.js
@@ -19,3 +19,26 @@ frm.addEventListener("submit", (e) => {
     }
     resp.innerText = resposta
 })
+/*B) Digamos que o número de chinchilas de uma fazenda triplica a cada ano, após o primeiro ano.
+Elaborar um programa que leia o número inicial de chinchilas e anos e informe ano a ano o número médio previsto 
+de chinchilas da fazenda. O número inicial de chinchilas deve ser maior ou igual a dois, um casal.*/
+// const resp = document.querySelector("pre")
+
+// frm.addEventListener("submit", (e) => {
+//     e.preventDefault()
+
+//     const chinchilas = Number(frm.inChinchilas.value)
+//     const anos = Number(frm.inAnos.value)
+
+//     let resposta = ""
+//     let previsao = chinchilas
+//     for (let i = 1; i <= anos; i++) {
+//         if (i == 1) {
+//             resposta = resposta + `${i}° Ano: ` + `${chinchilas}` + " Chinchilas" + "\n"
+//         } else {
+//             previsao *= 3
+//             resposta = resposta + `${i}° Ano: ` + `${previsao}` + " Chinchilas" + "\n"
+//         }
+//     }
+//     resp.innerText = resposta
+// }) 
