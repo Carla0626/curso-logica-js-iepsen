4751abc1e579b0b0e31c3a53bc4af71027c686a8
Carla Vitória <carlavitoria0626@gmail.com>
2026-03-25 11:13:14 -0300

    Add car registration and filtering functionality

---
+++ Aula JS/Cap 06/ex6_3.js
@@ -0,0 +1,47 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("pre")
+
+const carros = []
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const modelo = frm.inModelo.value
+    const preco = Number(frm.inPreco.value)
+
+    carros.push({ modelo, preco })  // adiciona dados ao vetor de objetos
+    frm.inModelo.value = ""
+    frm.inPreco.value = ""
+    inModelo.focus()
+
+    //dispara um evento de click em btListar (equivale um clique no botão da pagina)
+    frm.btListar.dispatchEvent(new Event("click"))
+})
+
+frm.btListar.addEventListener("click", () => {
+    if (carros.length == 0) {
+        alert("Não há carros na lista")
+        return
+    }
+    //metodo reduce() concatena uma string, obtendo modelo e preço de cada veiculos
+    const lista = carros.reduce((acumulador, carro) =>
+    acumulador + carro.modelo + " -R$ " + carro.preco.toFixed(2) + "\n" , "")
+    resp.innerText = `Lista dos Carros Cadastrados\n${"-".repeat(40)}\n${lista}` 
+})
+
+frm.btFiltrar.addEventListener("click", () => {
+    const maximo = Number(prompt("Qual o valor máximo que o cliente deseja pagar?"))
+    if (maximo == 0 || isNaN(maximo)) {
+        return
+    }
+    //cria um novo vetor com os objetos que atendem a condição de filtro
+    const carrosFilter = carros.filter(carro => carro.preco <= maximo)
+    if (carrosFilter.length == 0){
+        alert("Não há carros com preço inferior ou igual ao solicitado")
+        return
+    }
+    let lista = ""
+    for (const carro of carrosFilter){ // percorre cada elemento do array
+        lista += `${carro.modelo} - R$: ${maximo.toFixed(2)}\n`
+    }
+    resp.innerText = `Carros Até R$: ${maximo.toFixed(2)}\n${"-".repeat(40)}\n${lista}`
+})
