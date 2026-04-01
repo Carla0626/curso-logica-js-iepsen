0c29fd37585c8d567e7ba0dc676a4204b963618c
Carla Vitória <carlavitoria0626@gmail.com>
2026-04-01 17:06:16 -0300

    Refactor game table program and comment out sections

--- Aula JS/Exercicios/cap06_Exercicios/Exercicios.js
+++ Aula JS/Exercicios/cap06_Exercicios/Exercicios.js
@@ -102,3 +102,60 @@ frm.btMontar.addEventListener("click", () => {
 //     }
 // }
 // )
+// c) Elaborar um programa que leia nome e número de acertos de candidatos inscritos em um concurso. Listar os 
+//dados a cada inclusão. Ao clicar no botão Aprovados 2ª Fase, ler o número de acertos para aprovação dos 
+//candidatos para a 2ª fase do concurso, conforme ilustra a Figura 6.16. O programa deve, então, exibir os 
+//candidatos aprovados, ou seja, apenas os que obtiveram nota maior ou igual à nota informada. 
+//Exibir os candidatos aprovados em ordem decrescente de número de acertos. Caso nenhum candidato tenha sido 
+//aprovado, exibir mensagem.
+
+// const candidatos = []
+// frm.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     const nome = frm.inCandidato.value
+//     const acertos = Number(frm.inNumero.value)
+
+//     candidatos.push({ nome, acertos})
+   
+//     frm.inCandidato.value = ""
+//     frm.inNumero.value = ""
+//     frm.inCandidato.focus()
+
+//     frm.btListar.dispatchEvent(new Event("click"))
+// })
+
+// frm.btListar.addEventListener("click", () => {
+//     if (candidatos.length == 0){
+//         alert("Não há candidatos cadastrados")
+//         return
+//     }
+//     let lista = ""
+    
+//     for(const candidato of candidatos) {
+//         const { nome, acertos } = candidato
+//         lista += nome + " - " + acertos + " acertos\n"
+//     }
+
+//     resp.innerText = lista
+// })
+
+// frm.btAprovados.addEventListener("click", () => {
+//     if (candidatos.length == 0){
+//         alert("Não há candidatos cadastrados")
+//         return
+//     }
+//     const nota = Number(prompt("Qual a nota minima para a segunda fase?"))
+
+//     // const aprovados = []
+
+//     const aprovados = candidatos.filter(candidato => candidato.acertos >= nota)
+//     if (aprovados.length == 0){
+//         alert("Não há candidatos aprovados para a segunda fase")
+//         return
+//     }
+//     let listaAprovados = ""
+//     for (const aprovado of aprovados){ 
+//         listaAprovados += `${aprovado.nome} ` + " - " + `${aprovado.acertos}` + " acertos\n"
+//     }
+//     resp.innerText = listaAprovados
+// })
